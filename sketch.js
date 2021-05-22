9c049b3be2d8152a6d1d339870c0169444dbe5ab
MKAWRE950 <73824679+MKAWRE950@users.noreply.github.com>
2021-05-22 23:02:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+//const Render = Matter.Render;
+
+var ground, gameState, engine, world, dustbin, paper;
+function preload() {
+  dustbinimg = loadImage("dustbin.png");
+}
+function setup() {
+  createCanvas(800, 400);
+
+  rectMode(CENTER);
+
+  gameState = "start";
+
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+  dustbin = new DustBin(720, 390, 100, 10);
+  paper = new Paper(100, 300, 40);
+  ground = Bodies.rectangle(width / 2, 400, width, 10, {
+    isStatic: true,
+  });
+  World.add(world, ground);
+}
+
+function draw() {
+  if (gameState === "start") {
+    background("black");
+    
+    if (keyCode === UP_ARROW) {
+      gameState = "play";
+    }
+  }
+  if (gameState === "play") {
+    rectMode(CENTER);
+    background(0);
+
+    dustbin.display();
+    paper.display();
+    imageMode(CENTER);
+    image(dustbinimg, width - 77, 360, 180, 200);
+  }
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW && gameState === "play") {
+    Matter.Body.applyForce(paper.body, paper.body.position, {
+      x: 50,
+      y: -100,
+    });
+  }
+}
